c84699bf375cec8f2c85a8c3874f712bcd735a5e
PavanPuppala16 <pavankumarp@fixitytech.com>
2022-12-15 10:55:02 +0530

    firstcommit

---
+++ AngularCURDOperations/BsinessLogic/EmployeeContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularCURDOperations.BsinessLogic
+{
+    public class EmployeeContext:DbContext
+    {
+
+
+        public EmployeeContext(DbContextOptions<EmployeeContext>options):base(options)
+            {
+
+
+
+        }
+
+
+        public DbSet<TblEmployee> TblEmployee { get; set;}
+        public DbSet<TblDesignation> TblDesignation { get; set; }
+
+    }
+}
---
+++ AngularCURDOperations/BsinessLogic/TblEmployee.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+    namespace AngularCURDOperations.BsinessLogic
+{
+    public class TblEmployee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [StringLength(250)]
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public int age { get; set; }
+        public DateTime Doj { get; set; }
+        public string Gender { get; set; }
+        public int IsMarried { get; set; }
+        public int IsActive { get; set; }
+        public int DesignationID { get; set; }
+
+        [NotMapped]
+        public string Designation { get; set; }
+
+
+
+    }
+}
